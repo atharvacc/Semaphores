11667092ce67a91646d03e8e410c3b41c304a390
atharvacc <anchalke@ucdavis.edu>
2020-02-16 16:30:49 -0800

    Removed critical section

--- p3/libuthread/sem.c
+++ p3/libuthread/sem.c
@@ -12,78 +12,70 @@ struct semaphore {
 
 sem_t sem_create(size_t count)
 {
-	enter_critical_section();
+	
 	sem_t newSemaphore = malloc(sizeof(struct semaphore));
 	newSemaphore->count =count;
 	newSemaphore->BLOCKED = queue_create();
-	exit_critical_section();
+	
 	return newSemaphore;
 }
 
 int sem_destroy(sem_t sem)
 {
-	enter_critical_section();
+
 	if( sem==NULL || queue_length(sem->BLOCKED)!=0 ) {
-		exit_critical_section();
+		
 		return -1;
 	}
 	else{
 		queue_destroy(sem->BLOCKED);
 		free(sem);
 	}
-	exit_critical_section();
+	
 	return 0;
 }
 
 int sem_down(sem_t sem)
 {
-	enter_critical_section();
+	
 	if(sem == NULL){ 
-		exit_critical_section();
 		return -1;
 	}// If sem is NULL
 	else if(sem->count > 0){ 
 		sem->count--;
-		exit_critical_section();
 	}// If can decrement then decrement
 	else if (sem->count <= 0){
 		pthread_t* curTid = malloc(sizeof(pthread_t));
 		curTid = pthread_self();
 		queue_enqueue(sem->BLOCKED, curTid);
 		thread_block();
-		exit_critical_section();
+		
 	} // Else if count is 0 or negative then block and wait
 	return 0;
 }
 
 int sem_up(sem_t sem)
 {
-	enter_critical_section();
 	pthread_t* blockedTid = malloc(sizeof(pthread_t));
 	if(sem == NULL){ 
 		free(blockedTid);
-		exit_critical_section();
 		return -1;
 	}// If sem is null then return -1
 	
 	else if (queue_dequeue(sem->BLOCKED,blockedTid) == 0){
 		thread_unblock(blockedTid);
 		free(blockedTid);
-		exit_critical_section();
 	}// Then dequee was succesfull 
 	else{
 		free(blockedTid);
 		sem->count++;
-		exit_critical_section();
 	}
 	return 0;
 }
 
 int sem_getvalue(sem_t sem, int *sval)
 {
-	enter_critical_section();
 	if(sem == NULL || sval == NULL){
-		exit_critical_section();
 		return -1;
 	}
 	else if(sem->count > 0){
@@ -93,7 +85,6 @@ int sem_getvalue(sem_t sem, int *sval)
 	else if (sem->count == 0){
 		*sval = -1 * queue_length(sem->BLOCKED);
 	}
-	exit_critical_section();
 	return 0;
 }
 
